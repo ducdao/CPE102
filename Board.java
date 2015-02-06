5d185eb00deecaf3220da4d530d38a95dba299e4
Duc Dao <dldao@calpoly.edu>
2015-02-06 12:15:51 -0800

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,10 @@
+public class Board
+{
+    public void printBoard()
+    {
+        System.out.println(" __|__|__");
+        System.out.println(" __|__|__");
+        System.out.println("   |  |  ");
+    }
+    
+}
